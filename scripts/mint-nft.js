39273a464d8f4e8bf1411514b6b673abf7a804c1
そうまーる <soumar@soumarunoMacBook-Pro.local>
2022-04-11 20:03:34 +0900

    all commit

---
+++ scripts/mint-nft.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+const contract = require("../artifacts/contracts/NFT.sol/MyNFT.json");
+const contractAddress = "contract address";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  
+    //the transaction
+    const tx = {
+      'from': PUBLIC_KEY,
+      'to': contractAddress,
+      'nonce': nonce,
+      'gas': 500000,
+      'maxPriorityFeePerGas': 2999999987,
+      'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  
+    console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+  }
+
+  mintNFT("url in pinata");
